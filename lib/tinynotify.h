3110031c8b77df86b675a16a2a172220917cf3e5
Michał Górny <mgorny@gentoo.org>
2011-08-16 16:12:41 +0200

    Describe sessions thoroughly.

--- lib/tinynotify.h
+++ lib/tinynotify.h
@@ -7,6 +7,24 @@
 #define _TINYNOTIFY_H
 
 /**
+ * SECTION: session
+ * @short_description: API to deal with libtinynotify sessions
+ * @include: tinynotify.h
+ *
+ * All interaction with libtinynotify is associated with a single session,
+ * represented by the #NotifySession type. Before calling any of the library
+ * functions, one must instantiate a new session using notify_session_new().
+ * When done with it, one should free the session using notify_session_free().
+ *
+ * One must not pass custom, invalid or freed #NotifySession to any of
+ * the functions. Otherwise, the results are unpredictable (and a segfault is
+ * the most harmless of them). One must not attempt to modify or manually free
+ * a #NotifySession instance, and one shall not assume anything specific about
+ * its actual type. When in question, one should pass a pointer to it rather
+ * than casting #NotifySession to one.
+ */
+
+/**
  * NotifySession
  *
  * A type describing a basic tinynotify session. It holds the data necessary for
@@ -18,6 +36,11 @@
  */
 
 typedef void* NotifySession;
+
+/**
+ * SECTION: error
+ * @short_description: API to deal with libtinynotify errors
+ */
 
 /**
  * NotifyError
@@ -44,6 +67,9 @@ typedef enum {
  *
  * Create and initialize a new libtinynotify session.
  *
+ * This function always succeeds. If it is unable to allocate the memory,
+ * program execution will be aborted.
+ *
  * Returns: a newly-instantiated NotifySession
  */
 NotifySession notify_session_new(void);
@@ -54,6 +80,9 @@ NotifySession notify_session_new(void);
  *
  * Free a libtinynotify session. This handles disconnecting and other cleanup
  * as well.
+ *
+ * This function always succeeds. After a call to this function,
+ * a #NotifySession is no longer valid.
  */
 void notify_session_free(NotifySession session);
 
