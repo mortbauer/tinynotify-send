f108d7f3ea0095af9daa79429f42d2e2dd45fc85
Michał Górny <mgorny@gentoo.org>
2011-08-18 23:02:08 +0200

    Introduce the base for libtinynotify-cli.

---
+++ lib/tinynotify-cli.c
@@ -0,0 +1,7 @@
+/* libtinynotify
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#include "config.h"
+#include "tinynotify-cli.h"
---
+++ lib/tinynotify-cli.h
@@ -0,0 +1,11 @@
+/* libtinynotify
+ * (c) 2011 Michał Górny
+ * 2-clause BSD-licensed
+ */
+
+#ifndef _TINYNOTIFY_CLI_H
+#define _TINYNOTIFY_CLI_H
+
+#include <tinynotify.h>
+
+#endif
