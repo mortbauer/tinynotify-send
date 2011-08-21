c38f0c37e83c348ece9a6b1f121a0c1528c08415
Michał Górny <mgorny@gentoo.org>
2011-08-21 13:02:49 +0200

    Support building system-wide variant as well.

--- src/tinynotify-send.c
+++ src/tinynotify-send.c
@@ -8,29 +8,51 @@
 #include <tinynotify.h>
 #include <tinynotify-cli.h>
 
+#ifdef BUILDING_SYSTEMWIDE
+#	include <tinynotify-systemwide.h>
+#endif
+
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
 	NotifySession s;
 	Notification n;
+
+#ifdef BUILDING_SYSTEMWIDE
+	int use_systemwide = 1;
+#else
 	int use_systemwide = 0;
+#endif
+
+	int ret;
 
 	n = notification_new_from_cmdline(argc, argv,
 			PACKAGE_STRING, &use_systemwide);
 	if (!n)
 		return 1;
+
+#ifndef BUILDING_SYSTEMWIDE
 	if (use_systemwide) {
 		notification_free(n);
-		fputs("System-wide notification not supported.\n", stderr);
+		fputs("System-wide notifications not supported.\n", stderr);
 		return 1;
 	}
+#endif
 
 	s = notify_session_new("tinynotify-send", NOTIFY_SESSION_NO_APP_ICON);
-	if (notification_send(n, s))
+
+#ifdef BUILDING_SYSTEMWIDE
+	if (use_systemwide)
+		ret = notification_send_systemwide(n, s);
+	else
+#endif
+		ret = !notification_send(n, s);
+
+	if (!ret)
 		fprintf(stderr, "%s\n", notify_session_get_error_message(s));
 
 	notify_session_free(s);
 	notification_free(n);
 
-	return 0;
+	return !ret;
 }
